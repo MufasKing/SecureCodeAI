4a896a79d2728d9c61d93bd994e2e1dbcd17f662
Stefano Violante <ste.violante@gmail.com>
2024-07-20 10:55:57 +0200

    190 finished

---
+++ ChatGPT/190_3c.py
@@ -0,0 +1,18 @@
+def main():
+    # Declare a variable called totalSales
+    totalSales = 0
+
+    # Add sales for each month to the totalSales variable
+    january_sales = 1200
+    february_sales = 1500
+    march_sales = 1800
+
+    totalSales += january_sales
+    totalSales += february_sales
+    totalSales += march_sales
+
+    # Print the total sales to the screen
+    print("Total Sales:", totalSales)
+
+if __name__ == "__main__":
+    main()
